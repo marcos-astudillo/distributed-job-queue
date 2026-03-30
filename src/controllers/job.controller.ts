8dfdbc645f41eef4aec01db4aa714e88a228dcd8
Marcos Astudillo <antonio.ac86@hotmail.com>
2026-03-30 16:19:10 -0400

    feat: connect lease endpoint to Redis queue with delayed jobs handling

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -1,15 +1,17 @@
 import { FastifyRequest, FastifyReply } from "fastify";
 import { CreateJobRequest } from "../models/job.model";
 import { JobRepository } from "../repositories/job.repository";
+import { QueueRepository } from "../repositories/queue.repository";
 
 const jobRepo = new JobRepository();
+const queueRepo = new QueueRepository();
 
 export const createJob = async (
   request: FastifyRequest<{ Body: CreateJobRequest }>,
   reply: FastifyReply,
 ) => {
   const job = await jobRepo.createJob(request.body);
-
+  await queueRepo.enqueue(job.job_id);
   return reply.code(201).send(job);
 };
 
@@ -17,9 +19,14 @@ export const leaseJobs = async (
   request: FastifyRequest,
   reply: FastifyReply,
 ) => {
-  // TODO: conectar con queue
+  const { limit = 10 } = request.query as any;
+
+  await queueRepo.moveDelayedToReady();
+
+  const jobIds = await queueRepo.leaseJobs(limit);
+
   return {
-    jobs: [],
+    jobs: jobIds.map((id) => ({ job_id: id, visibility_timeout_sec: 30 })),
   };
 };
 
